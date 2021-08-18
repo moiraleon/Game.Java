95fc921dc5d238cfbf4f0a7e97bc5468e06c17c8
Moira Leon <moira.leon2@gmail.com>
2021-08-17 18:10:49 -0600

    greet player

---
+++ Game.java
@@ -0,0 +1,53 @@
+/*
+
+A number-guessing game.
+
+*/
+
+// public class Game {
+//   public static void main(String[] args){
+//     // Put your code here
+//   }
+
+// }
+// public class Game {
+//   public static void main(String[] args){
+//     System.out.println("Coke is better than Pepsi");
+//   }
+// }
+
+//Step 2 Results
+// moleon@MacBook-Pro guessing-game-java % javac Game.java
+// moleon@MacBook-Pro guessing-game-java % java Game
+// Coke is better than Pepsi
+
+
+//Step 3 build out guessing game
+//pseudo code
+// greet player
+// get player name
+// choose random number between 1 and 100
+// repeat forever:
+//     get guess
+//     if guess is incorrect:
+//         give hint
+//         increase number of guesses
+//     else:
+//         congratulate player
+ 
+//Greet Player
+
+// public class Game {
+//   public static void main(String[] args){
+//     System.out.println("Helloooo there! What is your name?");
+//   }
+// }
+
+//Get Players name
+
+public class Game {
+  public static void main(String[] args){
+    System.out.println("Coke is better than Pepsi");
+    
+  }
+}
